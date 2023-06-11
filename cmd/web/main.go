ed6685d057da4a44a41f106ebc33b171974321d9
Navneet shukla <navneetshukla824@gmail.com>
2023-06-11 21:41:40 +0530

    Learned Basic of routing and templates

---
+++ cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bookings-udemy/pkg/config"
+	"bookings-udemy/pkg/handlers"
+	"bookings-udemy/pkg/render"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main function
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	
+	srv:=&http.Server{
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+	err=srv.ListenAndServe()
+	log.Fatal(err)
+}
