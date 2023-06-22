08e52d8af480d0ab8e95db55c0ade2859fbb7f54
Navneet shukla <navneetshukla824@gmail.com>
2023-06-22 20:35:51 +0530

    Some changes

--- internal/render/render.go
+++ internal/render/render.go
@@ -17,6 +17,8 @@ var functions = template.FuncMap{}
 
 var app *config.AppConfig
 
+var pathToTemplate="./templates"
+
 // NewTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
 	app = a
@@ -65,7 +67,7 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	myCache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl",pathToTemplate))
 	if err != nil {
 		return myCache, err
 	}
@@ -77,13 +79,13 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 			return myCache, err
 		}
 
-		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl",pathToTemplate))
 		if err != nil {
 			return myCache, err
 		}
 
 		if len(matches) > 0 {
-			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl",pathToTemplate))
 			if err != nil {
 				return myCache, err
 			}
