976f406d0691f6a7363b2e73f442c19737627647
Navneet shukla <navneetshukla824@gmail.com>
2023-06-22 21:20:42 +0530

    Tested render method

---
+++ internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"bookings-udemy/internal/config"
+	"bookings-udemy/internal/models"
+	"encoding/gob"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	testApp.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app=&testApp
+
+}
