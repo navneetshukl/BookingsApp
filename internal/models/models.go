76f7332bc1f539e2ff9c8b3911979bd4e7858e97
Navneet shukla <navneetshukla824@gmail.com>
2023-07-01 14:29:23 +0530

    Added models for all the tables in our database

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 //Reservation holds reservation data
 
 type Reservation struct {
@@ -7,4 +9,63 @@ type Reservation struct {
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+//Users is user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	Created     time.Time
+	UpdatedAt   time.Time
+}
+
+//Rooms is room model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+//Restrictions is restriction model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the room restriction model
+
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomIDs       int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
