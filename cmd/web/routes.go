47316ab404177cfe64f1ea05aa48e139bb9ce94e
Navneet shukla <navneetshukla824@gmail.com>
2023-08-07 18:36:14 +0530

    Started writing code for admin dashboard

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -46,6 +46,9 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Use(Auth)
 
 		mux.Get("/dashboard",handlers.Repo.AdminDashboard)
+		mux.Get("/reservations-new",handlers.Repo.AdminNewReservations)
+		mux.Get("/reservations-all",handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar",handlers.Repo.AdminReservationsCalendar)
 	})
 
 	return mux
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -17,5 +17,7 @@ type DatabaseRepo interface {
 	GetUserByID(id int) (models.User, error)
 	UpdateUser(u models.User) error
 	Authenticate(email,testPassword string)(int,string,error)
+
+	AllReservations() ([]models.Reservation,error)
 }
 
